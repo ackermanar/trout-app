078aa16a477686467d646004f07419d3e95598de
AJ Ackerman <ajackerman12@icloud.com>
2024-01-30 11:28:50 -0500

    Integrate Shiny Dashboard

--- Script/kin_mapV1-app.R
+++ Script/kin_mapV1-app.R
@@ -1,53 +1,42 @@
 library(shiny)
+library(shinydashboard)
+library(data.table)
 
-ui <- fluidPage(
-  
-  # App title
-  titlePanel("Kinship Calculator"),
-  
-  # Sidebar layout with input and output definitions
-  sidebarLayout(
-    
-    # Sidebar panel for inputs
-    sidebarPanel(
-      
-      # Input: Selector for choosing file to upload
-      fileInput("file1", "Choose CSV File",
+ui <- dashboardPage(
+  dashboardHeader(title = "Kinship Calculator"),
+  dashboardSidebar(
+    sidebarMenu(
+      fileInput("file1", "Upload CSV File",
                 multiple = FALSE,
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
-      
-      # Input: Text input for search bar
-      textInput("search", "Search:")
-    ),
-    
-    # Main panel for displaying outputs
+      sidebarSearchForm(textId = "searchbar", buttonId = "searchbtn", label = "Search..."),
+      menuItem("Current state", tabName = "state", icon = icon("dashboard")),
+      menuItem("Stats", tabName="stats", icon = icon("bar-chart")),
+      menuItem("Help", tabName="help", icon = icon("question-circle"))
+    )
+  ),
+  dashboardBody(
     mainPanel(
-      
-      # Output: Data file summary
       tableOutput("contents")
     )
   )
 )
 
 server <- function(input, output) {
-  
-  # Load the uploaded file
   data <- reactive({
     file <- input$file1
     if(is.null(file)){return()} 
     read.csv(file$datapath)
   })
   
-  # Filter the data based on the search input
   filteredData <- reactive({
     data <- data()
     if(is.null(data)){return()}
-    data[grep(input$search, data$column_to_search),]
+    data[grep(input$searchbar, data$column_to_search),]
   })
   
-  # Display the filtered data
   output$contents <- renderTable({
     data <- filteredData()
     if(is.null(data)){return()}
@@ -55,5 +44,4 @@ server <- function(input, output) {
   })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
