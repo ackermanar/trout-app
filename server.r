1d2e4441a7a876de505c33c742575d58cb4fae3c
ackermanar <ajackerman12@icloud.com>
2024-10-02 09:40:37 -0400

    third working version of app

--- server.r
+++ server.r
@@ -1,211 +1,310 @@
+# List of required packages
+required_packages <- c("shiny", "shinydashboard", "shinyjs", "DT", "kinship2", "openxlsx", "tidyverse")
+
+# Function to check and install missing packages
+install_if_missing <- function(package) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+    library(package, character.only = TRUE)
+  }
+}
+
+# Check and install each package
+lapply(required_packages, install_if_missing)
+
 library(shiny)
 library(shinydashboard)
+library(shinyjs)
 library(DT)
 library(kinship2)
 library(openxlsx)
 library(tidyverse)
 
 server <- function(input, output, session) { # nolint
-observe({
+  observe({
+    output$message1 <- renderText({
+      if (is.null(input$pedigree_file)) {
+      return("Please upload a candidate file and a pedigree file.")
+      }
+    })
+
+    output$message2 <- renderText({
+      if (is.null(input$pedigree_file)) {
+        return("Please upload weights for length and weight.")
+      }
+    })
     # read in list of candidates with cols
     req(input$candidate_file)
-    candidate_file <- input$candidate_file
-    candidates <- read_table(candidate_file$datapath)
+    tryCatch({
+      candidates <- read_table(input$candidate_file$datapath)
 
-    # Make lists of males and females
-    males_to_select <- candidates %>%
-    subset(sex == "M") %>%
-    pull(id)
+      # Make lists of males and females
+      males_to_select <- candidates %>%
+        subset(sex == "M") %>%
+        pull(id)
 
-    females_to_select <- candidates %>%
-    subset(sex == "F") %>%
-    pull(id)
+      females_to_select <- candidates %>%
+        subset(sex == "F") %>%
+        pull(id)
+    }, error = function(e) {
+      output$message1 <- renderText({
+         paste("Please inspect the candidate file formatting, an error occurred:", e$message)
+      })
+    })
+
+    output$message1 <- renderText({
+      paste("Candidate file upload successful, please upload a pedigree file.")
+    })
+
+    output$message2 <- renderText({
+      paste("Candidate file upload successful, please upload weight and length files.")
+    })
 
     if (!is.null(input$pedigree_file)) {
-    pedigree_file <- input$pedigree_file
-    raw_ped <- read_table(pedigree_file$datapath) %>%
-        mutate(id = as.factor(id),
-        sire = as.factor(sire),
-        dam = as.factor(dam)
+      tryCatch({
+        raw_ped <- read_table(input$pedigree_file$datapath) %>%
+          mutate(id = as.factor(id),
+                 sire = as.factor(sire),
+                 dam = as.factor(dam)
+          )
+
+        sex_ped <- raw_ped %>%
+          mutate(sex = case_when(
+                 id %in% sire ~ 0,
+                 id %in% dam ~ 1,
+                 .default = 2
+          ))
+
+        #check for ids that appear as both sire and dam
+        messy_parents <- as.data.frame(intersect(sex_ped$sire, sex_ped$dam)) %>%
+          rename(id = 1) %>%
+          filter(id != 0)
+
+        #change values in messy_parents to 0 in cols for sire and dam
+        #create new datafram
+        parents_fixed_ped <- sex_ped
+        # Fix values in sire column
+        parents_fixed_ped$sire[parents_fixed_ped$sire %in% messy_parents[, 1]] <- 0
+        # Fix values in dam column
+        parents_fixed_ped$dam[parents_fixed_ped$dam %in% messy_parents[, 1]] <- 0
+
+        #check for duplicate IDs
+        n_occur <- data.frame(table(parents_fixed_ped$id)) %>%
+          rename(id = 1, freq = 2) #count occurrence of each ID
+
+        double_ids <- n_occur[n_occur$freq > 1,] #print ids with more than 1 occurrence
+
+        #create new dataframe without doubled ids(all instances are removed to avoid data errors)
+        nodup_ped <- parents_fixed_ped[!parents_fixed_ped$id %in% double_ids$id, ]
+
+        #check for individuals that appear as offspring and sire or offspring and dam of themselves
+
+        circ_deps <- nodup_ped %>%
+          mutate(id = as.character(id),
+                 sire = as.character(sire),
+                 dam = as.character(dam)) %>%
+          filter(id == sire | id == dam)
+
+        no_circ_deps <- anti_join(nodup_ped, circ_deps, by = "id")
+
+        #run FixPedigree to add parents that are not found as founders
+        ready_ped <- with(no_circ_deps, fixParents(id, sire, dam, sex, missid = "0"))
+
+        #create a pedigree object
+        final_ped <- with(ready_ped, pedigree(id, dadid, momid, sex, missid = "0"))
+
+        #calculate kinship matrix
+        kinship_matrix <- kinship(final_ped) #check Amatrix
+
+        selected_matrix <- kinship_matrix[males_to_select, females_to_select]
+
+        # Select rows and columns based on the list of ids
+        selected_matrix <- kinship_matrix[males_to_select, females_to_select]
+
+        # Add labels to the rows and columns
+        rownames(selected_matrix) <- paste("male", rownames(selected_matrix), sep = "_")
+        colnames(selected_matrix) <- paste("female", colnames(selected_matrix), sep = "_")
+      
+        quantiles_list <- list(
+          Q25 = quantile(selected_matrix, 0.25),
+          Q50 = quantile(selected_matrix, 0.50),
+          Q75 = quantile(selected_matrix, 0.75),
+          Q100 = quantile(selected_matrix, 1.00)
         )
 
-    sex_ped <-raw_ped %>%
-        mutate(sex = case_when(
-            id %in% sire ~ 0,
-            id %in% dam ~ 1,
-            .default = 2
-        ))
+        # Convert the list of quantiles to a tibble column
+        quadrants <- as_tibble(quantiles_list)
 
-    #check for ids that appear as both sire and dam
-    messy_parents <- as.data.frame(intersect(sex_ped$sire, sex_ped$dam)) %>%
-        rename(id = 1) %>%
-        filter(id != 0)
+        output$quadrants_table <- renderDT({
+          datatable(quadrants,
+                    options = list(ordering = FALSE, dom = 't'),
+                    rownames = FALSE) %>%
+          formatStyle(
+            columns = names(quadrants),
+            backgroundColor = styleInterval(
+              c(quantile(unlist(quadrants), 0.25),
+                quantile(unlist(quadrants), 0.50),
+                quantile(unlist(quadrants), 0.75)),
+              c('green', 'yellow', 'orange', 'red')  # Four colors for three intervals
+            )
+          )
+        })
 
-    #change values in messy_parents to 0 in cols for sire and dam
-    #create new datafram
-    parents_fixed_ped <- sex_ped
-    # Fix values in sire column
-    parents_fixed_ped$sire[parents_fixed_ped$sire %in% messy_parents[, 1]] <- 0
-    # Fix values in dam column
-    parents_fixed_ped$dam[parents_fixed_ped$dam %in% messy_parents[, 1]] <- 0
+        kinMat<- as_tibble(selected_matrix, rownames = "Row") %>%
+          pivot_longer(-Row, names_to = "Female", values_to = "Kinship") %>%
+          select(Female, Row, Kinship) %>%
+          rename(Male = Row) %>%
+          arrange(Kinship)
 
-    #check for duplicate IDs
-    n_occur <- data.frame(table(parents_fixed_ped$id)) %>%
-        rename(id = 1, freq = 2) #count occurrence of each ID
-
-    double_ids <- n_occur[n_occur$freq > 1,] #print ids with more than 1 occurrence
-
-    #create new dataframe without doubled ids(all instances are removed to avoid data errors)
-    nodup_ped <- parents_fixed_ped[!parents_fixed_ped$id %in% double_ids$id, ]
-
-#check for individuals that appear as offspring and sire or offspring and dam of themselves
-
-    circ_deps <- nodup_ped %>%
-        mutate(id = as.character(id),
-            sire = as.character(sire),
-            dam = as.character(dam)) %>%
-        filter(id == sire | id == dam)
-
-    no_circ_deps <- anti_join(nodup_ped, circ_deps, by = "id")
-
-    #run FixPedigree to add parents that are not found as founders
-    ready_ped <- with(no_circ_deps, fixParents(id, sire, dam, sex, missid = "0"))
-    
-    #create a pedigree object
-    final_ped <- with(ready_ped, pedigree(id, dadid, momid, sex, missid = "0"))
-
-    #calculate kinship matrix
-    kinship_matrix <- kinship(final_ped) #check Amatrix
-
-    selected_matrix <- kinship_matrix[males_to_select, females_to_select]
-
-    # Select rows and columns based on the list of ids
-    selected_matrix <- kinship_matrix[males_to_select, females_to_select]
-
-    # Add labels to the rows and columns
-    rownames(selected_matrix) <- paste("male", rownames(selected_matrix), sep = "_")
-    colnames(selected_matrix) <- paste("female", colnames(selected_matrix), sep = "_")
-
-    # Render tbl
-    output$matrix <- renderTable({
-        selected_matrix
-    }, rownames = TRUE)
-    }
-
-if (!is.null(input$weight_file) && !is.null(input$length_file)) {
-    #read in EBV files
-    weight_file <- input$weight_file
-    req(weight_file)
-    weight_ebvs <- read_table(weight_file$datapath)
-
-    length_file <- input$length_file
-    req(length_file)
-    length_ebvs <- read_table(length_file$datapath)
-
-    #### EBV calculations ####
-    # calculate index function
-    calculate_index <- function(...) {
-    args <- list(...)
-    
-    # Ensure the number of arguments is even (each dataset must have a corresponding weight)
-    if (length(args) %% 2 != 0) {
-        stop("Each EBV dataset must be followed by a corresponding weight.")
-    }
-    
-    # Separate the datasets and weights
-    ebv_list <- args[seq(1, length(args), by = 2)]
-    rel_weights <- unlist(args[seq(2, length(args), by = 2)])
-    
-    # Check that the sum of the weights is 1
-    if (sum(rel_weights) != 1.00) {
-        stop("The sum of relative weights must equal 1.00.")
-    }
-    
-    # Start with the first dataset in the list
-    joint_ebvs <- ebv_list[[1]]
-    
-    # Merge all EBV datasets by 'ID'
-    for (i in 2:length(ebv_list)) {
-        joint_ebvs <- merge(joint_ebvs, ebv_list[[i]], by = "ID", suffixes = c("", paste0(".", i)))
-    }
-    
-    # Calculate the index value
-    joint_ebvs$index_val <- 0
-    for (i in 1:length(rel_weights)) {
-        if (i == 1) {
-        ebv_col_name <- "EBV"
-        } else {
-        ebv_col_name <- paste0("EBV.", i)
+        # Render tbl
+        output$matrix <- renderDT({
+          datatable(kinMat, rownames = TRUE) %>%
+            formatStyle(
+              'Kinship',
+              backgroundColor = styleInterval(
+                c(quantile(kinMat$Kinship, 0.25),
+                  quantile(kinMat$Kinship, 0.50),
+                  quantile(kinMat$Kinship, 0.75)),
+                c('green', 'yellow', 'orange', 'red')  # Four colors for three intervals
+              )
+            )
+        })
+        if(!is.null(kinMat)) {
+          output$message1 <- renderText({
+            paste("Kinship matrix generated succesfully.")
+          })
         }
-        joint_ebvs$index_val <- joint_ebvs$index_val + (rel_weights[i] * joint_ebvs[[ebv_col_name]])
+      }, error = function(e) {
+        output$message1 <- renderText({
+         paste("Please inspect the pedigree file formatting, an error occurred:", e$message)
+        })
+      })
     }
-    
-    return(joint_ebvs)
+
+    if (!is.null(input$weight_file) && !is.null(input$length_file)) {
+      tryCatch({
+        #read in EBV files
+        req(input$candidate_file, input$weight_file, input$length_file)
+        weight_ebvs <- read_table(input$weight_file$datapath)
+        length_ebvs <- read_table(input$length_file$datapath)
+        testTot <- input$weight1 + input$weight2
+        
+        #### EBV calculations ####
+        # calculate index function
+        calculate_index <- function(...) {
+        args <- list(...)
+        
+        # Ensure the number of arguments is even (each dataset must have a corresponding weight)
+        if (length(args) %% 2 != 0) {
+          output$message2 <- renderText({
+            paste("Each EBV dataset must be followed by a corresponding weight.")
+          })
+          stop("Each EBV dataset must be followed by a corresponding weight.")
+        }
+        
+        # Separate the datasets and weights
+        ebv_list <- args[seq(1, length(args), by = 2)]
+        rel_weights <- unlist(args[seq(2, length(args), by = 2)])
+
+        # Start with the first dataset in the list
+        joint_ebvs <- ebv_list[[1]]
+        
+        # Merge all EBV datasets by 'ID'
+        for (i in 2:length(ebv_list)) {
+            joint_ebvs <- merge(joint_ebvs, ebv_list[[i]], by = "ID", suffixes = c("", paste0(".", i)))
+        }
+        
+        # Calculate the index value
+        joint_ebvs$index_val <- 0
+        for (i in 1:length(rel_weights)) {
+            if (i == 1) {
+            ebv_col_name <- "EBV"
+            } else {
+            ebv_col_name <- paste0("EBV.", i)
+            }
+            joint_ebvs$index_val <- joint_ebvs$index_val + (rel_weights[i] * joint_ebvs[[ebv_col_name]])
+        }
+        
+        return(joint_ebvs)
+        }
+
+        output$weightText1 <- renderText({
+        paste("EBV weight for fish weight:", input$weight1)
+        })
+
+        output$weightText2 <- renderText({
+        paste("EBV weight for fish length:", input$weight2)
+        })
+
+        weight_length_index <- calculate_index(weight_ebvs, input$weight1, length_ebvs, input$weight2)
+
+        # subset EBVs to those of candidates only and by sex
+        candidate_ebvs <- candidates %>%
+            left_join(weight_length_index, by = c("id" = "ID")) %>%
+        select(c(1,2,5))
+
+        male_candidate_ebvs <- candidate_ebvs[candidate_ebvs$id %in% males_to_select, ] %>% 
+        select(c(1,3)) %>%
+        mutate(
+            id = paste("male", id, sep = "_")
+        )
+        female_candidate_ebvs <- candidate_ebvs[candidate_ebvs$id %in% females_to_select, ] %>% 
+        select(c(1,3)) %>%
+        mutate(
+            id = paste("female", id, sep = "_")
+        )
+
+        # make matrix of EBVs
+        # Initialize an empty matrix
+        ebv_matrix <- matrix(NA, nrow = nrow(male_candidate_ebvs), ncol = nrow(female_candidate_ebvs))
+        rownames(ebv_matrix) <- male_candidate_ebvs$id
+        colnames(ebv_matrix) <- female_candidate_ebvs$id
+
+        # Populate the matrix with average EBV values
+        for(i in 1:nrow(male_candidate_ebvs)) {
+        for(j in 1:nrow(female_candidate_ebvs)) {
+            ebv_matrix[i, j] <- round(mean(c(male_candidate_ebvs$index_val[i], female_candidate_ebvs$index_val[j])),2)
+        }
+        }
+        output$matrix2 <- renderTable({
+            ebv_matrix
+        }, rownames = TRUE)
+        if (testTot != 1 && !is.null(ebv_matrix)) {
+          output$message2 <- renderText({
+            paste("Warning: EBV weights are not equal to 1, current value:", testTot)
+          })
+        } else if (!is.null(ebv_matrix) && testTot == 1) {
+          output$message2 <- renderText({
+            paste("EBV matrix generated succesfully.")
+          })
+        } 
+      } , error = function(e) {
+        output$message2 <- renderText({
+          paste("An error occurred:", e$message)
+        })
+      })
     }
 
-    output$weight1 <- renderText({
-    paste("EBV weight for fish weight:", input$weight1)
-    })
-
-    output$weight2 <- renderText({
-    paste("EBV weight for fish length:", input$weight2)
-    })
-
-    weight_length_index <- calculate_index(weight_ebvs, input$weight1, length_ebvs, input$weight2)
-
-    # subset EBVs to those of candidates only and by sex
-    candidate_ebvs <- candidates %>%
-        left_join(weight_length_index, by = c("id" = "ID")) %>%
-    select(c(1,2,5))
-
-    male_candidate_ebvs <- candidate_ebvs[candidate_ebvs$id %in% males_to_select, ] %>% 
-    select(c(1,3)) %>%
-    mutate(
-        id = paste("male", id, sep = "_")
+  output$download <- downloadHandler(
+      filename = function() {
+      paste0("trout_app_results-", Sys.Date(), ".xlsx")
+      },
+      content = function(file) {
+      wb <- createWorkbook()
+      if(!is.null(selected_matrix)) {
+      # Add a worksheet for the selected matrix
+      addWorksheet(wb, "Kinship Matrix")
+      writeData(wb, sheet = "Kinship Matrix", selected_matrix, rowNames = TRUE)
+      }
+      if(!is.null(ebv_matrix)) {
+      # Add a worksheet for the ebv matrix
+      addWorksheet(wb, "EBV Matrix")
+      writeData(wb, sheet = "EBV Matrix", ebv_matrix, rowNames = TRUE)
+      }
+      # Save the workbook to a file
+      saveWorkbook(wb, file, overwrite = TRUE)
+      }
     )
-    female_candidate_ebvs <- candidate_ebvs[candidate_ebvs$id %in% females_to_select, ] %>% 
-    select(c(1,3)) %>%
-    mutate(
-        id = paste("female", id, sep = "_")
-    )
-
-    # make matrix of EBVs
-    # Initialize an empty matrix
-    ebv_matrix <- matrix(NA, nrow = nrow(male_candidate_ebvs), ncol = nrow(female_candidate_ebvs))
-    rownames(ebv_matrix) <- male_candidate_ebvs$id
-    colnames(ebv_matrix) <- female_candidate_ebvs$id
-
-    # Populate the matrix with average EBV values
-    for(i in 1:nrow(male_candidate_ebvs)) {
-    for(j in 1:nrow(female_candidate_ebvs)) {
-        ebv_matrix[i, j] <- round(mean(c(male_candidate_ebvs$index_val[i], female_candidate_ebvs$index_val[j])),2)
-    }
-    }
-    output$matrix2 <- renderTable({
-        ebv_matrix
-    }, rownames = TRUE)
-    }
-
-output$download <- downloadHandler(
-    filename = function() {
-    paste0("trout_app_results-", Sys.Date(), ".xlsx")
-    },
-    content = function(file) {
-    wb <- createWorkbook()
-    if(!is.null(selected_matrix)) {
-    # Add a worksheet for the selected matrix
-    addWorksheet(wb, "Kinship Matrix")
-    writeData(wb, sheet = "Kinship Matrix", selected_matrix, rowNames = TRUE)
-    }
-    if(!is.null(ebv_matrix)) {
-    # Add a worksheet for the ebv matrix
-    addWorksheet(wb, "EBV Matrix")
-    writeData(wb, sheet = "EBV Matrix", ebv_matrix, rowNames = TRUE)
-    }
-    # Save the workbook to a file
-    saveWorkbook(wb, file, overwrite = TRUE)
-    }
-  )
-}) %>%
-    bindEvent(input$pedigree_file, input$candidate_file, input$download, input$weight_file, input$length_file)
+  }) %>%
+      bindEvent(input$pedigree_file, input$candidate_file, input$download, input$weight_file, input$weight1, input$length_file, input$weight2)
 }
--- ui.r
+++ ui.r
@@ -1,5 +1,20 @@
+# List of required packages
+required_packages <- c("shiny", "shinydashboard", "shinyjs", "DT", "kinship2", "openxlsx", "tidyverse")
+
+# Function to check and install missing packages
+install_if_missing <- function(package) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+    library(package, character.only = TRUE)
+  }
+}
+
+# Check and install each package
+lapply(required_packages, install_if_missing)
+
 library(shiny)
 library(shinydashboard)
+library(shinyjs)
 library(DT)
 library(kinship2)
 library(openxlsx)
@@ -10,27 +25,32 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       h4("Upload candidate mates"),
-      fileInput("candidate_file", "Upload available parents in csv format", accept = c(".csv", ".txt")),
+      fileInput("candidate_file", "Upload candidate parents in csv format", accept = c(".csv", ".txt")),
       h4("Calculate kinship matrix"),
       fileInput("pedigree_file", "Choose pedigree file, e.g. 'even_year_ped.txt'",
                 accept = ".txt"),
       h4("Calculate EBVs"),
       fileInput("weight_file", "Upload weights", accept = c(".csv", ".txt")),
-      numericInput("weight1", "EBV weight for fish weight:", 0.5, min = 0, max = 1),
+      numericInput("weight1", "EBV weight for fish weight:", 0.5, min = 0, max = 1, step = 0.1),
       verbatimTextOutput("weight1"),
       fileInput("length_file", "Upload length", accept = c(".csv", ".txt")),
-      numericInput("weight2", "EBV weight for fish length:", 0.5, min = 0, max = 1),
+      numericInput("weight2", "EBV weight for fish length:", 0.5, min = 0, max = 1, step = 0.1),
       h4("Download results"),
       downloadButton("download", label = "Download")
     ),
     mainPanel(
       tabsetPanel(
         tabPanel("Parents",
-          tableOutput("matrix")
+          h5("User feedback:"),
+          verbatimTextOutput("message1"),
+          DTOutput("quadrants_table"),
+          DTOutput("matrix")
         ),
         tabPanel("EBVs",
-          verbatimTextOutput("weight1"),
-          verbatimTextOutput("weight2"),
+          h5("User feedback:"),
+          verbatimTextOutput("message2"),
+          verbatimTextOutput("weightText1"),
+          verbatimTextOutput("weightText2"),
           tableOutput("matrix2")
         )
       )
